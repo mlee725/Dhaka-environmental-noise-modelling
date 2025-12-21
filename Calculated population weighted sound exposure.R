584f78478a187857b40bcf53ab83ab4c176ec165
mlee725 <martha.lee@mail.mcgill.ca>
2025-12-21 13:09:48 -0500

    Update Calculated population weighted sound exposure.R

--- Calculated population weighted sound exposure.R
+++ Calculated population weighted sound exposure.R
@@ -26,74 +26,73 @@ source("~/Source script for exposure analysis data.R")
 #################
 ## function to match raster grid resolutions of population and sound predictions and then get median exposure with population grid cells
 extractedpopsound <- function(pop_raster, mask_vector, sound_raster){
-  pop_raster_masked <- mask(x = pop_raster, mask = mask_vector) # to reduce the size of the population raster and cut computing time
+  pop_raster_masked <- mask(x = pop_raster, mask = mask_vector)
   sound_resampled <- resample(sound_raster, pop_raster_masked, method = "med")
   sound_resampled <- mask(x = sound_resampled, mask = mask_vector)
-  extracted <- terra::zonal(sound_resampled, pop_raster_masked, fun="median", na.rm=T, touches=FALSE, as.raster=T)
-  test <- crds(extracted, df=T)
   extracted2 <- terra::zonal(sound_resampled, pop_raster_masked, fun="median", na.rm=T, touches=FALSE, as.raster=F)
-  x <- cbind(test, extracted2)
-  return(x)
-}
-
-## creates cumulative density plots - I pulled some of this code from this source: https://rpubs.com/clement_hironimus/probdist
-cumden_plot <- function(exposure, pop, type, pop2){ # type either "Total" or "Stratified"
-  # Expand to individual exposure values
-  expanded_data <- rep(exposure, pop)
-  expanded_df <- data.frame(exposure = expanded_data)
-  sample.range <- floor(min(expanded_df$exposure)):ceiling(max(expanded_df$exposure))
-  iq.mean <- mean(expanded_df$exposure)
-  iq.sd <- sd(expanded_df$exposure)
-  cdf <- pnorm(sample.range, iq.mean, iq.sd)
-  iq.dist <- dnorm(sample.range, mean = iq.mean, sd = iq.sd)
-  iq.df <- data.frame("sound" = sample.range, "Density" = iq.dist)
-  iq.df <- cbind(iq.df, "CDF_LowerTail" = cdf)
-  if(type == "Total"){
-    ## just one curve for total population
-    plot <- ggplot() +
-      stat_ecdf(data=expanded_df, aes(exposure), alpha=0.8, lty="11", color = "#E7B800") +
-      geom_line(data=iq.df, aes(sound, CDF_LowerTail), lty="solid", color = "#E7B800", linewidth=0.8) +
-      labs(
-        title = "",
-        x = expression(paste("Average ", LA[eq], " (dBA) exposure")),
-        y = "Cumulative propertion of population exposed"
-      ) +
-      theme_classic() 
-  }else if(type == "Stratified"){
-    ## add in additional population
-    expanded_data2 <- rep(exposure, pop2)
-    expanded_df2 <- data.frame(exposure = expanded_data2)
-    sample.range2 <- floor(min(expanded_df2$exposure)):ceiling(max(expanded_df2$exposure))
-    iq.mean2 <- mean(expanded_df2$exposure)
-    iq.sd2 <- sd(expanded_df2$exposure)
-    cdf2 <- pnorm(sample.range2, iq.mean2, iq.sd2)
-    iq.dist2 <- dnorm(sample.range2, mean = iq.mean2, sd = iq.sd2)
-    iq.df2 <- data.frame("sound" = sample.range2, "Density" = iq.dist2)
-    iq.df2 <- cbind(iq.df2, "CDF_LowerTail" = cdf2)
-    
-    plot <- ggplot() +
-      stat_ecdf(data=expanded_df, aes(exposure), alpha=0.8, lty="11", color = "#E7B800") +
-      geom_line(data=iq.df, aes(sound, CDF_LowerTail), lty="solid", color = "#E7B800", linewidth=0.8) +
-      stat_ecdf(data=expanded_df2, aes(exposure), alpha=0.8, lty="11", color = "#00AFBB") +
-      geom_line(data=iq.df2, aes(sound, CDF_LowerTail), lty="solid", color = "#00AFBB",  linewidth=0.8) +
-      labs(
-        title = "",
-        x = expression(paste("Average ", LA[eq], " (dBA) exposure")),
-        y = "Cumulative % of population exposed"
-      ) +
-      theme_classic() 
-  }
-  return(plot)
+  return(extracted2)
 }
 
 ##################
-## population analysis =====
-##################
+# -- A -- #
+# population level exposure
 
+# extract with rasters
 extractedpopsound_all <- extractedpopsound(WorldPop2020, adim2022, noise_all_masked)
 extractedpopsound_day <- extractedpopsound(WorldPop2020, adim2022, noise_day_masked)
 extractedpopsound_night <- extractedpopsound(WorldPop2020, adim2022, noise_night_masked)
+extractedpopsound_den <- extractedpopsound(WorldPop2020, adim2022, noise_den_masked)
 
-## cumulative density plots at grid-level
-total_plot <- cumden_plot(extractedpopsound$pred_overall_noise, extractedpopsound$bgd_ppp_2020_UNadj, "Total")
-total_plot
+# calculate pop exposure
+types <- list(df1 = extractedpopsound_all, 
+              df2 = extractedpopsound_day, 
+              df3 = extractedpopsound_night, 
+              df4 = extractedpopsound_den)
+extract_df <- data.frame()
+for (i in c(1:length(types))){
+  pull <- rep(types[[i]][,2], types[[i]][,1]) %>%
+    as.data.frame() %>%
+    summary() %>%
+    as.data.frame() %>%
+    dplyr::select(Var2, Freq) %>%
+    rename(stat = Var2,
+           vals = Freq) %>%
+    mutate(type = sub(".*?_(.*?)_.*", "\\1", names(types[[i]])[2]))
+  extract_df <- rbind(extract_df, pull[,2:3])
+}
+
+##################
+# -- B -- #
+# cumulative density plots at grid 
+
+expanded_data_night <- rep(extractedpopsound_night$pred_night_noise, extractedpopsound_night$bgd_ppp_2020_UNadj)
+expanded_n_df <- data.frame(exposure = expanded_data_night)
+
+expanded_data_day <- rep(extractedpopsound_day$pred_day_noise, extractedpopsound_day$bgd_ppp_2020_UNadj)
+expanded_d_df <- data.frame(exposure = expanded_data_day)
+
+expanded_data_all <- rep(extractedpopsound_all$pred_overall_noise, extractedpopsound_all$bgd_ppp_2020_UNadj)
+expanded_a_df <- data.frame(exposure = expanded_data_all)
+
+expanded_data_den <- rep(extractedpopsound_den$pred_den_noise, extractedpopsound_den$bgd_ppp_2020_UNadj)
+expanded_den_df <- data.frame(exposure = expanded_data_den)
+
+plot <- ggplot() +
+  stat_ecdf(data=expanded_n_df, aes(exposure), alpha=0.8, lty="solid", color = "#E2D200", linewidth=1.1) +
+  stat_ecdf(data=expanded_d_df, aes(exposure), alpha=0.8, lty="solid", color = "#46ACC8", linewidth=1.1) +
+  stat_ecdf(data=expanded_den_df, aes(exposure), alpha=0.8, lty="solid", color = "#E58601", linewidth=1.1) +
+  stat_ecdf(data=expanded_a_df, aes(exposure), alpha=0.8, lty="solid", color = "#B40F20", linewidth=1.1) +
+  geom_vline(xintercept =55, lty="dashed", color = "#E2D200", linewidth=0.7) +
+  geom_vline(xintercept =45, lty="dashed", color = "#46ACC8", linewidth=0.7) +
+  geom_vline(xintercept =53, lty="dashed", color = "#E58601", linewidth=0.7) +
+  geom_vline(xintercept =70, lty="dashed", color = "#29211F", linewidth=0.7) +
+  labs(
+    title = "",
+    x = "Sound in dBA",
+    y = "Cumulative proportion of population exposed"
+  ) +
+  theme(axis.title.x = element_text(size = 12), 
+        axis.title.y = element_text(size = 12)) +
+  theme_classic() #+ xlim(c(50,84))
+quartz()
+plot
